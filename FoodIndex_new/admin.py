12593732daa2f1d7392bf96a3e2d078424f3847b
Vedansh Kawadkar <67778396+VedanshKumarKawadkar@users.noreply.github.com>
2021-08-23 21:23:41 +0530

    Delete FoodIndex_new directory

--- FoodIndex_new/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from FoodIndex_new.models import *
-# Register your models here.
-
-admin.site.register(UploadCSV_new)
--- FoodIndex_new/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FoodindexNewConfig(AppConfig):
-    name = 'FoodIndex_new'
