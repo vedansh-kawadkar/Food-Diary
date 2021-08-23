769794b46ba04ca3271a876437567d4892fc2f5f
Vedansh Kawadkar <67778396+VedanshKumarKawadkar@users.noreply.github.com>
2021-08-23 21:33:16 +0530

    Add files via upload

---
+++ basic_app_new/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from basic_app_new.models import *
+# Register your models here.
+admin.site.register(Food_diary_new)
+admin.site.register(meal_new)
+admin.site.register(Transaction_det_new)
+admin.site.register(Temporary_new)
+admin.site.register(Unsaved_new)
+admin.site.register(Purchase_det_new)
+admin.site.register(Unsaved_purchase_new)
+admin.site.register(Temporary_purchase_new)
+admin.site.register(purchase_cards)
